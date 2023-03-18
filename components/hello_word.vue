8c25090cb5c7466487c6f49b815b55c1e60a74c9
unknown <HeinHtet>
2023-03-18 22:39:32 +0630

    fix

---
+++ components/hello_word.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>Hello WORLD</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
---
+++ pages/about.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>Hello De</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
